825fcead0844f1297985b1f4f06b0ad4787ee7b1
Adrian Sims <adriansims62781@gmail.com>
2021-01-12 10:57:42 +0100

    Add project files.

---
+++ XP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Threading.Tasks;
+
+namespace XP1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var doc = XDocument.Parse("<Root><a><b>Fred</b></a></Root>");
+                var v = doc.XPathSelectElement("//Root/a/b").Value;
+                Console.WriteLine(v);
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+    }
+}
